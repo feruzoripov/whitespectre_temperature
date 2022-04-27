84ff0c360349728538c991f5d66d8dc7a1315eb5
Feruz Oripov <feruzoripov@protonmail.com>
2022-04-27 23:59:02 +0500

    adding mailer

---
+++ app/models/temperature_mailer.rb
@@ -0,0 +1,16 @@
+class TemperatureMailer < MandrillMailer::MessageMailer
+  default from: 'support@example.com'
+
+  def send_alert(email, temperature)
+    email_info = {
+      subject: 'Exceed 37.5C',
+      to: email,
+      text: "Your temperature exceeded 37.5C. Your current temperature is #{temperature}C.",
+      html: "<p>Your temperature exceeded 37.5C. Your current temperature is #{temperature}C.</p>",
+      view_content_link: true,
+      important: true,
+      inline_css: true,
+    }
+    mandrill_mail 
+  end
+end

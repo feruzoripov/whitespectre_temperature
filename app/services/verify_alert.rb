de61098b11255c273682ec474a2ba03c30c1cc8b
Feruz Oripov <feruzoripov@protonmail.com>
2022-04-28 12:20:29 +0500

    changes

--- app/services/verify_alert.rb
+++ app/services/verify_alert.rb
@@ -1,11 +1,11 @@
 class VerifyAlert
-  LIMIT_TEMPERATURE = SystemConfig['limit_temperature'] || 37.5
+  LIMIT_TEMPERATURE = (SystemConfig['limit_temperature'] || 37.5).to_f
   EMAIL = 'test@example.com'
 
   def self.call
-    tmp = Temperature.first(5).sum(&:value)
-    if  (tmp / 5) > LIMIT_TEMPERATURE.to_f
-      TemperatureMailer.send_alert(EMAIL, tmp / 5).deliver_now
+    tmp = Temperature.first(5).sum(&:value) / 5 # trending temperature
+    if  tmp > LIMIT_TEMPERATURE
+      TemperatureMailer.send_alert(EMAIL, tmp).deliver_now
     end
   end
 end
